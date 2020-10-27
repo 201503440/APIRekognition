48339665bcfb46cc108dcb7e0b772c31ce5a1daf
Cesar Sazo <2295156810113@ingenieria.usac.edu.gt>
2020-10-27 12:09:40 -0600

    Credenciales

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 CORS(app)
 
 client = boto3.client('rekognition',
-                    aws_access_key_id='AKIAZITK2UZ7FGV3WUKW',
-                    aws_secret_access_key='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',
+                    aws_access_key_id=base64.b64decode("QUtJQVpJVEsyVVo3RkdWM1dVS1c=").decode("utf-8"),
+                    aws_secret_access_key=base64.b64decode("YVlBcHRvaGdNMUk5azBObWlEZW9zaXVmeXZuTlVwMDVJbm9jMXY3aA==").decode("utf-8"),
                     region_name='us-east-2')
 
 @app.route('/getLabels', methods=['GET', 'POST'])
